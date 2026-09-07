f6d7fab137f4d80dd1453b88859b7f2726d2495a
sam8457 <64925821+sam8457@users.noreply.github.com>
2026-09-07 14:37:58 -0500

    Remove redundant local variables from Job.jsx

--- src/components/Job.jsx
+++ src/components/Job.jsx
@@ -4,30 +4,24 @@ import Textbox from './Textbox.jsx'
 
 function Job({id, job, updateSingleJob}) {
 
-  const [title, setTitle] = useState(job.title);
-
   return (
     <>
       <hr />
       <Textbox
         label={"Job Title"}
-        value={title}
+        value={job.title}
         onChange={
           (input) => {
-            //console.log('changed title');
-            setTitle(input)
             updateSingleJob(
               id, 
               {
                 ...job,
                 title: input
-              }
-            )
-            //console.log(job)
-          }
-        }
+              })}}
       />
-      <Textbox label={"Company"} />
+      <Textbox 
+        label={"Company"} 
+      />
       <Textbox label={"Date Started"} />
       <Textbox label={"Date Ended"} />
     </>
