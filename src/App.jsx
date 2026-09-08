539be25936fefcad4b199451a750eaa72ff5f0c1
sam8457 <64925821+sam8457@users.noreply.github.com>
2026-09-07 22:03:16 -0500

    Change jobsList to Object of Objects

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,11 @@ import Job from './components/Job.jsx'
 import Edu from './components/Edu.jsx'
 import ResumePopup from './components/ResumePopup.jsx'
 
+function getLastKey(obj){
+  let lastKey = Object.keys(obj)[Object.keys(obj).length - 1] 
+  return lastKey;
+}
+
 function App() {
   
   // Todo: use definitve id's for children
@@ -13,46 +18,46 @@ function App() {
   // Todo: add remove button on per-job/edu basis
 
   const [jobsList, jobsUpdater] = useState(
-    [
-      {
+    {
+      0:{
         title: '',
         company: '',
         workStartDate: '',
         workEndDate: '',
       },
-    ]);
+    });
 
   function updateSingleJob(id, newJob) {
-    let newJobs = [...jobsList]
-    newJobs[id] = newJob
-    jobsUpdater(newJobs)
+    let newJobs = {...jobsList};
+    newJobs[id] = newJob;
+    jobsUpdater(newJobs);
   };
 
   function addJob() {
-    //const id = crypto.randomUUID();
-    jobsUpdater([...jobsList, {}]);
+    const id = crypto.randomUUID();
+    jobsUpdater({...jobsList, id:{}});
     console.log("add job");
   };
 
-  
-  function removeJob(removedJob) {
-    const newJobs = jobsList.slice(0,jobsList.length - 1);
+  function removeJob(removedId) {
+    let newJobs = jobsList;
+    delete newJobs[removedId];
     jobsUpdater(newJobs);
     console.log("removeJob")
+    console.log(jobsList);
   };
 
   const [edusList, edusUpdater] = useState(
-    [
-      {
+      {0:{
         degree: '',
         school: '',
         eduStartDate: '',
         eduEndDate: '',
-      },
-    ]);
+      },}
+    );
 
   function updateSingleEdu(id, newEdu) {
-    let newEdus = [...edusList]
+    let newEdus = {...edusList}
     newEdus[id] = newEdu
     edusUpdater(newEdus)
   };
@@ -90,26 +95,26 @@ function App() {
         <Textbox label='Email' value={email} onChange={setEmail}/>
 
         <h2 className='sectionHeader'>Work Experience</h2>
-        {jobsList.map(
-          (job, index) => {return(
+        {Object.entries(jobsList).map(
+          ([id, job]) => {return(
             <Job
-            key={index}
-            id={index}
+            key={id}
+            id={id}
             job={job}
             updateSingleJob={updateSingleJob}/>
           )}
         )}
         <div id="jobButtons">
-          <button onClick={removeJob}>Remove</button>
+          <button onClick={() => removeJob(getLastKey(jobsList))}>Remove</button>
           <button onClick={addJob}>Add</button>
         </div>
-
+ 
         <h2 className='sectionHeader'>Education</h2>
-        {edusList.map(
-          (edu, index) => {return(
+        {Object.entries(edusList).map(
+          ([id, edu]) => {return(
             <Edu
-            key={index}
-            id={index}
+            key={id}
+            id={id}
             edu={edu}
             updateSingleEdu={updateSingleEdu}/>
           )}
--- src/components/ResumePopup.jsx
+++ src/components/ResumePopup.jsx
@@ -22,18 +22,18 @@ function ResumePopup(
         <p>{email}</p>
 
         <h2 className='child'>Work Experience</h2>
-        {jobs.map( job => {
+        {Object.entries(jobs).map(([id, job]) => {
           return(
-            <div>
+            <div key={id}>
               <p className='child'>{job.workStartDate} - {job.workEndDate}</p>
               <p className='child'>{job.title} at {job.company}</p>
             </div>
           );
         })}
         <h2 className='child'>Education</h2>
-        {edus.map( edu => {
+        {Object.entries(edus).map(([id, edu]) => {
           return(
-            <div>
+            <div key={id}>
               <p className='child'>{edu.eduStartDate} - {edu.eduEndDate}</p>
               <p className='child'>{edu.degree} at {edu.school}</p>
             </div>
